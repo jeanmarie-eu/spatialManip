cc3ca02c52beae3b569ec01ddb621487856ca417
jeanmarielepioufle <jm@nexmodeling.com>
2017-04-06 18:26:36 +0200

    add case when information is cell.centered

--- R/spatial2grid.R
+++ R/spatial2grid.R
@@ -12,6 +12,6 @@
 #' }
 spatial2grid <- function(obj,cellsize=NULL,...) {
   x <- buildGridTopo(obj=obj,cellsize=cellsize,...)
-  spdf <- construct(type="grid",x=x,proj4S=sp::CRS(sp::proj4string(obj)))
+  spdf <- construct(type="grid",x=x,proj4S=crs(proj4string(obj)))
   return(spdf)
 }
--- R/xGridBuild.R
+++ R/xGridBuild.R
@@ -6,13 +6,20 @@
 #' @param ymin ymin
 #' @param ymax ymax
 #' @param cellsize cellsize
+#' @param CELLCENTER boolean
 #' @keywords spatialManip
 #' @export
 #' @examples
 #' \dontrun{
 #' xGridBuild(...)
 #' }
-xGridBuild <- function(xmin,xmax,ymin,ymax,cellsize){
+xGridBuild <- function(xmin,xmax,ymin,ymax,cellsize,CELLCENTER=FALSE){
+  if (CELLCENTER) {
+    xmin <- xmin - (cellsize/2)
+    xmax <- xmax + (cellsize/2)
+    ymin <- ymin - (cellsize/2)
+    ymax <- ymax + (cellsize/2)
+  }
   dimX <- round((xmax-xmin)/cellsize)
   dimY <- round((ymax-ymin)/cellsize)
   x <- list(cellcentre.offset=c((xmin + cellsize/2), (ymin - cellsize/2)), cellsize=c(cellsize,cellsize), cells.dim=c(dimX,dimY))
